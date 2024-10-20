79a84b892878b4bb00f1ca139543dbc2348d10c0
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-10-20 21:03:20 +0200

    📄 file: rename discount file

--- src/app/src/main/java/org/example/interfaces/Discountable.java
+++ src/app/src/main/java/org/example/interfaces/Discountable.java
@@ -4,11 +4,11 @@ package org.example.interfaces;
  * The Discount interface provides a method to apply a discount.
  * Implementing classes should define the logic for applying the discount.
  */
-public interface Discount
+public interface Discountable
 {
     /**
      * Applies a discount to the implementing class.
      * The specific discount logic should be defined in the implementing class.
      */
-    public void applyDiscount();
+    public int applyDiscount(int price);
 }
