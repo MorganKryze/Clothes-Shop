5c184253b1ad7e47c7fed7f8192bbf545e832921
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-10-20 21:27:01 +0200

    📄 file: format discount interface

--- src/app/src/main/java/org/example/interfaces/Discountable.java
+++ src/app/src/main/java/org/example/interfaces/Discountable.java
@@ -4,8 +4,7 @@ package org.example.interfaces;
  * The Discount interface provides a method to apply a discount.
  * Implementing classes should define the logic for applying the discount.
  */
-public interface Discountable
-{
+public interface Discountable {
     /**
      * Applies a discount to the implementing class.
      * The specific discount logic should be defined in the implementing class.
